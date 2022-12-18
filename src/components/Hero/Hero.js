2a5247e863cfd56995da1fe8e4b912e0c4ee92e8
Miraj Pudasaini <pudasainimetal96@gmail.com>
2022-12-18 00:31:22 +0000

    'learn_more_button_update'

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React , {useState} from 'react';
+import Collapsible from 'react-Collapsible'
 
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
+const [learnMore,setLearnMore]=useState(false);
+const {text}='As one of my favourite programming languages and toolkit for data engineering. <n/> I have hands on project done individually on python (not my course work). I recently revisited pandas library following a certification form kaggle.<n/> I have hands on experience with Databricks, Azure Data lake, Azure cosomoDB.'
 const Hero = (props) => (
   <div>
     <Section row nopadding >
@@ -15,11 +18,11 @@ const Hero = (props) => (
           Data Engineer with a passion for tech and all things tech. I recently graduated my masters 
           with first class. My major was big data and artificial intellegence following that I am on a lookout 
           for positions in data engineering. 
-          As one of my favourite programming languages and toolkit for data engineering. I have hands on project done 
-          individually on python (not my course work). I recently revisited pandas library following a certification form kaggle.
-          I have hands on experience with Databricks, Azure Data lake, Azure cosomoDB. <br/>
+          <n/>
+           <br/>
         </SectionText>
-        <Button onClick={() => window.location = " "}>Learn More</Button>
+        {learnMore ? text: '${text.substring(0,250)}'}
+        <Button onClick={() => setLearnMore(!learnMore)= " "}>Learn More</Button>
       </LeftSection>
     </Section>
   </div>
