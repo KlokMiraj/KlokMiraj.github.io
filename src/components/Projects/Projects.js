3d3a8df9c51f40762e571a9e9603f6133b47c722
Miraj Pudasaini <pudasainimetal96@gmail.com>
2022-12-19 13:43:51 +0000

    'github_api_integration'

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,16 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+import { useEffect, useState } from 'react';
+
+const[githubData,setGithubData]=useState([]);
+const[githubUser,setGithubUser]=useState("klokmiraj")
+
+const fetchData=()=>{
+  return fetch('https://api.github.com/users/klokMiraj}')
+  .then((response)=> response.json())
+  .then((data)=>setGithubData(data));
+}
 const Projects = () => (
   <div>
     <Section nopadding id='projects'>
