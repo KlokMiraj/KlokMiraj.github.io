c6687adc3d8660f33d158b0c1105114f6d3f2fde
klokMiraj <pudasainimetal96@gmail.com>
2023-12-18 05:09:11 +0000

    build fix

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/twitter_api":["static\u002Fchunks\u002Fpages\u002Ftwitter_api.js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftwitter_api"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
